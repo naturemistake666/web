a8a4f407bee7e44e83c799907f0e75e138429797
naturemistake666 <65819256+naturemistake666@users.noreply.github.com>
2022-06-02 13:52:11 +0300

    Commit

---
+++ src/main/java/com/webUser/web/service/Service.java
@@ -0,0 +1,16 @@
+package com.webUser.web.service;
+
+import com.webUser.web.model.User;
+
+import java.util.List;
+
+public interface Service {
+
+    Object findById(Long id);
+
+    List<User> findAll();
+
+    void saveUser(User user);
+
+    void deleteById(Long id);
+}
--- src/main/java/com/webUser/web/service/UserService.java
+++ src/main/java/com/webUser/web/service/UserService.java
@@ -2,12 +2,13 @@ package com.webUser.web.service;
 
 import com.webUser.web.model.User;
 import com.webUser.web.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
